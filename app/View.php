c43d391a6973b03293102bd92a651c1f954de1ca
abumaryam <wiradianxiv@mti.gadjahmada.edu>
2018-06-04 00:01:23 +0700

    agar layout dapat dinamis

--- app/View.php
+++ app/View.php
@@ -1,22 +1,23 @@
-<?php 
+<?php
 
 namespace app;
 
 /**
-* Root View
-*/
-class View
-{
-	public static function render($view,$params=array())
-	{
+ * Root View
+ */
+class View {
+
+	public static $layout = 'content';
+
+	public static function render($view, $params = array()) {
 		ob_start();
 		if (!empty($params)) {
 			foreach ($params as $key => $value) {
 				$$key = $value;
 			}
 		}
-		include_once 'view/'.$view.'.php';
+		include_once 'view/' . $view . '.php';
 		$content = ob_get_clean();
-		include_once 'template/content.php';
+		include_once 'template/' . self::$layout . '.php';
 	}
 }
